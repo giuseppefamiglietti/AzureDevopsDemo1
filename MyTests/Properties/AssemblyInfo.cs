660e2cb5277a3bdfbf53b20069deb6d939a7eb87
peppo81 <peppe.famiglietti@gmail.com>
2019-05-08 18:29:38 +0200

    aggiunto nuovo progetto

---
+++ MyTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("MyTests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f1c6ba40-329c-47f8-b878-5ca0ca024019")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MyTests/Test_PlayerCharacterShould.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using GameEngine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyTests
+{
+    [TestClass]
+    public class Test_PlayerCharacterShould
+    {
+        //verifico che il parametro is Noob è true quando creo un nuovo PlayerCharacter
+        [TestMethod]
+        public void BeInexperiencedWhenNew()
+        {
+            var sut = new PlayerCharacter();
+
+            Assert.IsFalse(sut.IsNoob);
+        }
+    }
+}
